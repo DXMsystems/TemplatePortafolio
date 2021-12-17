fc47e7e47f12d30031b3445d921f11c3a5da7d89
DrJzzz <josue9499@gmail.com>
2021-12-17 02:56:45 -0600

    added material ui library and types

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Button from '@mui/material/Button';
 
 ReactDOM.render(
   <React.StrictMode>
