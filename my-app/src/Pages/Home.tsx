6f96b8bf3be4e814d12ddf1c8b676dcb916a0f53
jrgels <jrgels99@gmail.com>
2021-12-26 03:49:49 -0600

    Se agrego el video a home

--- my-app/src/Pages/Home.tsx
+++ my-app/src/Pages/Home.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import Header from "../Components/Header/Header";
+//@ts-ignore
+import video from "../Pages/materials/hacker.mp4";
+
 const Home = () => {
   return (
     <div>
       homehomehome
+      <video autoPlay loop muted
+      style={{
+        position:"absolute",
+        width:"100%",
+        left:"50%",
+        top:"50%",
+        height:"100%",
+        objectFit:"cover",
+        transform:"translate(-50%, -50%)",
+        zIndex: "-1"
+      }}>
+        <source src={video} type="video/mp4"/>
+      </video>
     </div>
   )
 };
