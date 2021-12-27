ff4e413a65fd538b7c4b57dacf7c33e771d3b45a
DrJzzz <josue9499@gmail.com>
2021-12-26 21:24:09 -0600

    moved cdn imports to index.html

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -7,17 +7,6 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-      crossOrigin="anonymous"
-    />
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-      crossOrigin="anonymous"
-    />
     <BrowserRouter>
       <App />
     </BrowserRouter>
