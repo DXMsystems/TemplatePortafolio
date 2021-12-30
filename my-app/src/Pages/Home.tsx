4ab5d857083daf0544ad8907b577d124940f279e
jrgels <jrgels99@gmail.com>
2021-12-30 04:12:36 -0600

    Se agrego el efecto typing

--- my-app/src/Pages/Home.tsx
+++ my-app/src/Pages/Home.tsx
@@ -3,13 +3,19 @@ import Header from "../Components/Header/Header";
 //@ts-ignore
 import video from "../Pages/materials/hacker.mp4";
 import "./Home.css"
+import "./typingEfect/app"
+import "./typingEfect/style.css"
 
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="text-container">
-      <h1 className="texto">Nombre Apellido</h1>
+      <div className="name-container">
+      <h1 className="texto">Jorge Sanchez</h1>
       </div>
+        <div className="hero">
+          <h1 className="title">I'm a</h1>
+          <h1 className="typewrite"></h1>
+        </div>
       <video autoPlay loop muted className="background-video">
         <source src={video} type="video/mp4"/>
       </video>
