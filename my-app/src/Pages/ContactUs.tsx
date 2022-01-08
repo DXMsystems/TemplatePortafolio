d13e70006f2b8797505d46ff885247eae416c7a5
jrgels <jrgels99@gmail.com>
2022-01-08 02:17:20 -0600

    Se arreglo el rpoblema de contact us ¡Ya carga!

--- my-app/src/Pages/ContactUs.tsx
+++ my-app/src/Pages/ContactUs.tsx
@@ -1,7 +1,6 @@
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import ContactForm from "../Components/ContactForm/ContactForm";
 import SectionTitle from "../Components/SectionTitle/SectionTitle";
-import "./ContactUs.css";
 import ContactInfo from "../Components/ContactInfo/ContactInfo";
 
 export default function ContactUs() {
