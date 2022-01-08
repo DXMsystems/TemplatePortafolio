fb47d801809e95356a7a4151ca5951ece8d0961e
jrgels <jrgels99@gmail.com>
2022-01-08 02:01:11 -0600

    Se termino el front de ContactUs, ya esta totalmente refactorizado

--- my-app/src/Components/ContactInfo/ContactInfo.tsx
+++ my-app/src/Components/ContactInfo/ContactInfo.tsx
@@ -1,28 +1,20 @@
-import React from 'react'
-import { Col, Row } from 'react-bootstrap'
-import "./ContactInfo.css"
-import "../SectionTitle/SectionTitle.css"
+import { Col, Row } from "react-bootstrap";
+import "./ContactInfo.css";
+import "../SectionTitle/SectionTitle.css";
 
 //@ts-ignore
 export default function ContactInfo(props) {
-    return (
-        <>
-        <h1 className="subtitulo">Contact Info</h1>
-          <p className="contactText">
-            Always available for freelance work if the right project comes
-            along, Feel free to contact me!
-          </p>
-          <Row>
-            <Col className="borde__contact-item" xs={1}>
-            
-            </Col>
-            <Col xs={11}>
-              <h1 className="item__info-item">{props.titulo}</h1>
-              <p className="itemDescription">{props.texto}</p>
-            </Col>
-            
-          </Row>
-          </>
-    )
+  return (
+    <>
+      <Row>
+        <div className="diviciondeAhuevo">
+        <i className={"iconos bi "+ props.iconClass}></i>
+        </div>
+        <Col className="borde__contact-item" xs={10}>
+          <h1 className="item__info-item">{props.titulo}</h1>
+          <p className="itemDescription">{props.texto}</p>
+        </Col>
+      </Row>
+    </>
+  );
 }
-            
--- my-app/src/Pages/ContactUs.tsx
+++ my-app/src/Pages/ContactUs.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import ContactForm from "../Components/ContactForm/ContactForm";
 import SectionTitle from "../Components/SectionTitle/SectionTitle";
@@ -19,8 +18,20 @@ export default function ContactUs() {
         </Col>
 
         <Col>
-          <ContactInfo titulo="Nombre" texto="Jorge Sanchez"/>
+          <h1 className="subtitulo">Contact Info</h1>
+          <p className="contactText">
+            Always available for freelance work if the right project comes
+            along, Feel free to contact me!
+          </p>
           
+          <ContactInfo iconClass="bi-file-person-fill" titulo="Nombre" texto="Jorge Sanchez"/>
+          <ContactInfo
+            iconClass="bi-geo-alt-fill"
+            titulo="Location"
+            texto="4155 Mann Island, Liverpool, United Kingdom."
+          />
+          <ContactInfo iconClass="bi-send" titulo="Email me" texto="jrgels99@gmail.com"/>
+          <ContactInfo iconClass="bi-linkedin" titulo="Linkedin" texto="Check out my Linkedin"/>
         </Col>
       </Row>
     </Container>
