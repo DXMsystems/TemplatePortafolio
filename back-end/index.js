a54d317b759161b1e12b3af3c2ae9f142515ead9
DrJzzz <josue9499@gmail.com>
2022-01-11 03:52:02 -0600

    now the backend listens to post request for the contact us page

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const res = require('express/lib/response');
 const app = express();
 const PORT = 8000;
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/', (req,res) => res.send('test'));
+
+app.post('/contact-us', (req, res) => {
+  console.log(req.body)
+  res.status(201).send('expects JSON with name, email, subj, msg')
+})
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
+
