79e730bba4245a9996bd1984fc11940fe513537e
DrJzzz <josue9499@gmail.com>
2022-01-11 15:21:17 -0600

    adds validation for contact post request

--- back-end/index.js
+++ back-end/index.js
@@ -1,19 +1,29 @@
-const express = require('express');
-const res = require('express/lib/response');
+const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const PORT = 8000;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+const { body, validationResult } = require("express-validator");
 
-app.get('/', (req,res) => res.send('test'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.post('/contact-us', (req, res) => {
-  console.log(req.body)
-  res.status(201).send('expects JSON with name, email, subj, msg')
-})
+app.get("/", (req, res) => res.send("test"));
+
+app.post(
+  "/contact-us",
+  body("name").isLength({ min: 1 }).isAlphanumeric(),
+  body("email").isEmail(),
+  body("sub").isAlphanumeric().isLength( {min : 1} ),
+  body("msg").isLength( { min: 10, max: 500 } ),
+  (req, res) => {
+    errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
-
