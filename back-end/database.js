0a77c90c19be6e30c180dbcb384ff4d51f72a201
DrJzzz <josue9499@gmail.com>
2022-01-24 04:03:29 -0600

    adds script that generets the contact db

---
+++ back-end/database.js
@@ -0,0 +1,32 @@
+var sqlite3 = require('sqlite3').verbose()
+
+const DBSOURCE = "db.sqlite"
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+      // Cannot open database
+      console.error(err.message)
+      throw err
+    }else{
+        console.log('Connected to the SQLite database.')
+        db.run(`CREATE TABLE Contact (
+            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            name TEXT NOT NULL, 
+            email TEXT NOT NULL, 
+            subject TEXT NOT NULL,
+            message TEXT NOT NULL, 
+            date DATE NOT NULL
+            );`,
+        (err) => {
+            if (err) {
+                console.log("Table already created")
+            }else{
+                // Table just created, creating some rows
+                console.log("B")
+            }
+        });  
+    }
+});
+
+
+module.exports = db
