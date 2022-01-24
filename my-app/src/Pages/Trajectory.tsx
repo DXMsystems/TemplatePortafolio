6e6fcb3a1df9c8c1f6ba2f5cdc927672e3c43f36
jrgels <jrgels99@gmail.com>
2022-01-24 04:50:19 -0600

    Se empezo la plantilla de tecnologias en Trajectory

--- my-app/src/Pages/Trajectory.tsx
+++ my-app/src/Pages/Trajectory.tsx
@@ -1,44 +1,70 @@
 import { Card, Col, Row } from "react-bootstrap";
 import SectionTitle from "../Components/SectionTitle/SectionTitle";
-import "./Trajectory.css"
+import "./Trajectory.css";
 import TrajectoryCard from "../Components/TrajectoryCard/TrajectoryCard";
+import Subsection from "../Components/Subsection/Subsection";
+import TrajectorySkills from "../Components/TrajectorySkills/TrajectorySkills";
+
 
 export default function Trajectory() {
   return (
     <>
       <div className="container">
-        <SectionTitle descripcion="Check out my Trajectory" title="Trajectory"/>
+        <SectionTitle
+          descripcion="Check out my Trajectory"
+          title="Trajectory"
+        />
+      
+      <TrajectorySkills url="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg" cardTitle="Bootstrap" cardText="High"/>
 
+
+        <Subsection
+          description="Some of my achievements"
+          title=" Resume"
+        />
         <div className="ed-ex"></div>
         <Row>
           <Col>
-          <h1 className="Col-tit">Education</h1>
+            <h1 className="Col-tit">Education</h1>
           </Col>
           <Col>
-          <h1 className="Col-tit">Experience</h1>
+            <h1 className="Col-tit">Experience</h1>
           </Col>
         </Row>
 
         <Row>
           <Col>
-            {/* <div>
-              <Card className="card-tr">
-                <Card.Body>
-                  <span className="item-arrow"></span>
-                  <Card.Title className="cardTit-tr">Computer Science</Card.Title>
-                  <Card.Subtitle className="cardSub-tr">UNAM 2018 - 4eva</Card.Subtitle>
-                  <Card.Text className="cardtxt-tr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quo repudiandae.</Card.Text>
-                </Card.Body>
-              </Card>
-            </div> */}
-            <TrajectoryCard tit="Computer Engineering" sub="UNAM 2018-2022" txt="No aprendi ni berga jeje xD"/>
-
+            <TrajectoryCard
+              tit="Computer Engineering"
+              sub="UNAM 2018-2022"
+              txt="No aprendi ni berga jeje xD"
+            />
           </Col>
           <Col>
-            
+            <TrajectoryCard
+              tit="Prepa 6"
+              sub="UNAM 2018-2022"
+              txt="No aprendi ni berga jeje xD"
+            />
           </Col>
         </Row>
-        
+
+        <Row>
+          <Col>
+            <TrajectoryCard
+              tit="Camara que digo?"
+              sub="UNAM 2018-2022"
+              txt="No aprendi ni berga jeje xD"
+            />
+          </Col>
+          <Col>
+            <TrajectoryCard
+              tit="Jeje te menti, no tengo experiencia xD"
+              sub="UNAM 2018-2022"
+              txt="No aprendi ni berga jeje xD"
+            />
+          </Col>
+        </Row>
       </div>
     </>
   );
