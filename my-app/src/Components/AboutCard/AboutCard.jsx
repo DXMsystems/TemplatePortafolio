ddf8c87092bf4d550dc9d8c17d403685c3b7dee3
DrJzzz <josue9499@gmail.com>
2022-01-25 02:54:39 -0600

    fix hieght disparity between cards in about us page

--- my-app/src/Components/AboutCard/AboutCard.jsx
+++ my-app/src/Components/AboutCard/AboutCard.jsx
@@ -4,14 +4,14 @@ import "./AboutCard.css";
 //@ts-ignore
 export default function AboutCard(props) {
   return (
-    <div className="service">
-      <Card className="card">
+
+      <Card className="card service">
         <i className={"service-icon " + props.iconClass}></i>
         <Card.Body>
           <Card.Title className="card-title">{props.cardTitle}</Card.Title>
           <Card.Text className="card-text">{props.cardText}</Card.Text>
         </Card.Body>
       </Card>
-    </div>
+   
   );
 }
