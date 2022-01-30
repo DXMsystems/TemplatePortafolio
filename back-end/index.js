197f50a5b40c101bd6d159ded678c709de231237
DrJzzz <josue9499@gmail.com>
2022-01-30 02:28:45 -0600

    contact request now adds data to the db

--- back-end/index.js
+++ back-end/index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const { body, validationResult } = require("express-validator");
+const db = require("./database.js");
 
-const cors = require('cors')
+const nodemailer = require("nodemailer");
+
+const cors = require("cors");
 const app = express();
 const PORT = 8000;
 
-
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/about-me', express.static('assets'));
+app.use("/about-me", express.static("assets"));
 
 app.get("/", (req, res) => res.send("test"));
 
@@ -25,8 +27,38 @@ app.post(
     errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
-    } else{
-      return res.status(201).end("Well done hoe!")
+    } else {
+      let current = new Date();
+      let cDate =
+        current.getFullYear() +
+        "-" +
+        (current.getMonth() + 1) +
+        "-" +
+        current.getDate();
+      let cTime =
+        current.getHours() +
+        ":" +
+        current.getMinutes() +
+        ":" +
+        current.getSeconds();
+      let dateTime = cDate + " " + cTime;
+
+      const data = {
+        name: req.body.name,
+        email: req.body.email,
+        subject: req.body.sub,
+        message: req.body.msg,
+        date: dateTime,
+      };
+      const sql ='INSERT INTO Contact (name, email, subject, message, date) VALUES (?,?,?,?,?)'
+      const params = [data.name, data.email, data.subject, data.message, data.date]
+      db.run(sql, params, (err, result) => {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+    });
+      return res.status(201).end("Well done hoe!");
     }
   }
 );
