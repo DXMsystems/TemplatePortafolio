77895043a82ca60da031cd4732e66efac327a12a
jrgels <jrgels99@gmail.com>
2022-02-17 19:30:25 -0600

    Ahora se le pueden poner enlaces en la pagina Contact

--- my-app/src/Components/ContactInfo/ContactInfo.tsx
+++ my-app/src/Components/ContactInfo/ContactInfo.tsx
@@ -4,8 +4,8 @@ import "../SectionTitle/SectionTitle.css";
 
 //@ts-ignore
 export default function ContactInfo(props) {
-  const link = !props.mail;
-
+  const mail = props.mail;
+  const link = props.link;
   return (
     <>
       <Row>
@@ -14,12 +14,16 @@ export default function ContactInfo(props) {
         </div>
         <Col className="borde__contact-item" xs={10}>
           <h1 className="item__info-item">{props.titulo}</h1>
-          {link ? (
-            <p className="itemDescription">{props.texto}</p>
-          ) : (
+          {mail ? (
             <a href="mailto:jrgels99@gmail.com" className="itemDescription">
               {props.texto}
             </a>
+          ) : link ? (
+            <a href={props.link} className="itemDescription">
+              {props.texto}
+            </a>
+          ) : (
+            <p className="itemDescription">{props.texto}</p>
           )}
         </Col>
       </Row>
